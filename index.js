55e2e84041ee97b125a331fda9a5e88487a7d78d
ADITYA SHARMA <61984836+AdityaSharma29@users.noreply.github.com>
2024-03-16 16:46:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-
-
 app.get("/", (req, res) => {
     return res.json(users);
 });
 
+app.get("/:id", (req, res) => {
+    
+    for(var i = 0; i < users.length; i++) {
+        if(users[i].id == req.params.id) {
+            return res.json({"name": users[i].name});
+        }
+    }
+    return res.json({"error": "User not found. Kindly enter ID between 1001 and 1050."});
+});
+
 app.listen(PORT, () => console.log("Server Live!!"));
-
-
